5df549c4675ad16b27cc59e9f54eeb104d60ae57
Otávio Alencar <oalencardev@gmail.com>
2024-08-29 20:37:19 -0300

    fix:page export bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from '@/components/sections/hero/hero'
 import { headerLinks } from '@/data/headerLinks'
 
-export const Page = () => {
+const Page = () => {
   return (
     <div>
       <Hero className="container mt-10" links={{ data: headerLinks }} />
